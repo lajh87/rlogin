39039c7fa0e372ed36340fd60624c46c865ff082
lheley <lajh87@live.co.uk>
2023-05-15 14:04:31 -0400

    update

--- tests/testthat/test-verify-password.R
+++ tests/testthat/test-verify-password.R
@@ -1,8 +1,10 @@
 test_that("Verify Password", {
+  create_dummy_user()
+
   input <- list(
     username = "testuser",
     password = "Password1!"
   )
 
-  expect_true(verify_password())
+  expect_true(verify_password(input$username, input$password))
 })
